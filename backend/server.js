f9d427a6399be3ccbbc349aada987fea1d902e92
Mariam123-Git <mkone9489@gmail.com>
2025-08-31 09:01:09 +0100

    Delete backend directory

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-// server.js
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
